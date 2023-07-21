4dd2d35f96d8443716f0cd31a2b39b3d0a55ddbc
adji142 <prasetyoajiw@gmail.com>
2023-07-21 15:45:38 +0700

    fixing dashboard

--- application/controllers/C_Patroli.php
+++ application/controllers/C_Patroli.php
@@ -50,7 +50,7 @@
 			$SQL2 = "
 				SELECT 
 					a.LocationID,
-					COALESCE((TIMESTAMPDIFF(MINUTE, d.MulaiBekerja, d.SelesaiBekerja) * COUNT(DISTINCT a.KodeCheckPoint)) / (d.IntervalPatroli * 60),0) JumlahRencanaPatroli,
+					CASE WHEN (d.IntervalPatroli * 60) =0 THEN 0 ELSE COALESCE((TIMESTAMPDIFF(MINUTE, d.MulaiBekerja, d.SelesaiBekerja) * COUNT(DISTINCT a.KodeCheckPoint)) / (d.IntervalPatroli * 60),0) END JumlahRencanaPatroli,
 					COUNT(b.id) JumlahPatroliAktual
 				FROM tcheckpoint a
 				LEFT JOIN patroli b on a.KodeCheckPoint = b.KodeCheckPoint AND a.LocationID = b.LocationID AND a.RecordOwnerID = b.RecordOwnerID 
