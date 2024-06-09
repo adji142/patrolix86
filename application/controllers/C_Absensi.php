bafdc12f1531134cb3a039e5b6f2da4f548bb755
adji142 <prasetyoajiw@gmail.com>
2024-06-09 09:48:06 +0700

    Update C_Absensi.php test 2

--- application/controllers/C_Absensi.php
+++ application/controllers/C_Absensi.php
@@ -485,7 +485,9 @@
                     $dateTo->modify('1 days');
                 }
                 $dateTo->modify('-30 minutes');
-
+		echo "CurentDate: ".$curentDate ."<br>";
+		    echo "Tgl Awal: ".$datefrom ."<br>";
+		    echo "Tgl Akhir: ".$dateTo ."<br>";
                 if ($currentDate >= $datefrom && $currentDate <= $dateTo) {
                     // echo $key->NamaShift."<br>";
                     $isGantiHari = $key->GantiHari;
